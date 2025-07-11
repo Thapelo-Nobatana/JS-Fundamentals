15f089b410bd73fa556662a192ce414865a54929
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-07-11 12:37:35 +0200

    Developed a 3-value_argument.js file

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,6 +2,8 @@ const args = process.argv.slice(2);
 
 if(args[0] === undefined) {
     console.log("No argument");
-} else {
-    console.log(`${args}`)
+} else if(args[0] !== undefined) {
+    let sentence = args.join(' ');
+    sentence = sentence.replace(/,/g, ' ');
+    console.log(`${sentence} `)
 }
